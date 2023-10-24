1b4dc4c0b92662db0e13ba015cea65eb26d8de65
blamine19 <47577101+blamine19@users.noreply.github.com>
2023-10-24 11:51:20 +0200

    Update create.component.js

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom"
 export default function CreateProduct() {
 
     /********* amine belh */
+    /*** safo **/
     
     const navigate = useNavigate();
 
